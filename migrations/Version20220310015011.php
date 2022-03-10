db3b42800b4d73499ca4a0b31bff1ce960873b8b
Moetaz-doghman <61354236+Moetaz-doghman@users.noreply.github.com>
2022-03-10 02:59:27 +0100

    up

--- migrations/Version20220310015011.php
+++ migrations/Version20220310015011.php
@@ -10,11 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-<<<<<<< HEAD:migrations/Version20220310012254.php
-final class Version20220310012254 extends AbstractMigration
-=======
-final class Version20220310003147 extends AbstractMigration
->>>>>>> 920fc3e155be89e097721f4538a7a3ff289ff7ea:migrations/Version20220310003147.php
+final class Version20220310015011 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -24,20 +20,14 @@ final class Version20220310003147 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-<<<<<<< HEAD:migrations/Version20220310012254.php
-        $this->addSql('ALTER TABLE utilisateurs CHANGE age age VARCHAR(255) NOT NULL');
-=======
         $this->addSql('ALTER TABLE boutique ADD latitude VARCHAR(255) NOT NULL, ADD longitude VARCHAR(255) NOT NULL');
->>>>>>> 920fc3e155be89e097721f4538a7a3ff289ff7ea:migrations/Version20220310003147.php
+        $this->addSql('ALTER TABLE utilisateurs ADD age VARCHAR(255) NOT NULL, CHANGE telephone telephone VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-<<<<<<< HEAD:migrations/Version20220310012254.php
-        $this->addSql('ALTER TABLE utilisateurs CHANGE age age VARCHAR(255) DEFAULT NULL');
-=======
         $this->addSql('ALTER TABLE boutique DROP latitude, DROP longitude');
->>>>>>> 920fc3e155be89e097721f4538a7a3ff289ff7ea:migrations/Version20220310003147.php
+        $this->addSql('ALTER TABLE utilisateurs DROP age, CHANGE telephone telephone INT NOT NULL');
     }
 }
